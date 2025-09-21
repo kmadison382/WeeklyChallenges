74a8e11e44de7e1f0ee9f00f9f5cc3108afec07c
Krystal Madison <k.madison.382@gmail.com>
2025-09-21 12:27:32 -0500

    completed set 6

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -7,27 +7,122 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            var containsWord = false;
+
+            if (words == null)
+            {
+                return containsWord;
+            }
+
+            foreach (var item in words)
+            {
+                if (item == null)
+                {
+                    continue;
+                }
+
+                if (!ignoreCase)
+                {
+                    if (item == word)
+                    {
+                        containsWord = true;
+                    }
+                }
+                else
+                {
+                    if (item.ToLower() == word.ToLower())
+                    {
+                        containsWord = true;
+                    }
+                }
+            }
+
+            return containsWord;
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num < 2)
+            {
+                return false;
+            }
+
+            var isPrime = true;
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                }
+            }
+
+            return isPrime;
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            var uIndex = -1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                var unique = true;
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (str[i] == str[j] && i != j)
+                    {
+                        unique = false;
+                    }
+                }
+
+                if (unique)
+                {
+                    uIndex = i;
+                }
+            }
+
+            return uIndex;
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            var count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var countTwo = 1;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        countTwo++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (countTwo > count)
+                {
+                    count = countTwo;
+                }
+            }
+
+            return count;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            if (elements == null || elements.Count == 0 || n < 1)
+            {
+                return new double[] {};
+            }
+
+            List<double> nthElements = new List<double>();
+            
+            for (int i = n - 1; i < elements.Count; i+=n)
+            {
+                nthElements.Add(elements[i]);
+            }
+            return nthElements.ToArray();
         }
     }
 }
